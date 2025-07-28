c3788326edab9c8d8d1e711aae8441fd5288cd3f
JANE-dot978 <gathujane97@gmail.com>
2025-07-29 00:13:20 +0300

    added a video and text overlay on home page, still on progress

--- pawradise-event-system/src/components/Home.js
+++ pawradise-event-system/src/components/Home.js
@@ -1,5 +1,21 @@
+import React from "react";
+
+import video from "../components/dogs vid.mp4"; 
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="video-container">
+      <video autoPlay loop muted className="bg-video">
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="overlay-text">
+        <h1>Welcome to the Dog Event System</h1>
+        <p>Book events, meet pet lovers, and enjoy!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
