91db1f6429827c711e9494ee7a889f8a397d511b
JANE-dot978 <gathujane97@gmail.com>
2025-08-17 21:48:53 +0300

    made the contact form hold 2 sections, logo,form and more details about the system

--- pawradise-event-system/src/components/Contact.js
+++ pawradise-event-system/src/components/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
     setStatus(null);
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("http://localhost:4000/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
